82ea0f6458977f2af411b396884d00e118deb6e7
tywi6665 <tyletcole@gmail.com>
2020-05-26 12:02:31 -0600

    created container component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Container from './Container';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-
+      <Container>
+        <p>This is a React App!!</p>
+      </Container>
     </div>
   );
 }
---
+++ src/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Container = ({ children }) => {
+    return (
+
+        <div className="wrapper">
+            {children}
+        </div>
+
+    );
+}
+
+export default Container;
